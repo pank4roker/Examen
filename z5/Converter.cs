e580c5f6837e366b1cf25f47de7a036e0e360c07
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-05-03 12:45:12 +0300

    Add files via upload

---
+++ z5/Converter.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z5
+{
+    class Converter
+    {
+        double usd;
+        double eur;
+        double rub;
+
+        public Converter(double usd, double eur, double rub)
+        {
+            Usd = usd;
+            Eur = eur;
+            Rub = rub;
+        }
+        public Converter() { }
+
+        public double Usd { get => usd; set => usd = value; }
+        public double Eur { get => eur; set => eur = value; }
+        public double Rub { get => rub; set => rub = value; }
+
+        public void Exchange(double kursrub,double kursusd,double kurseur)
+        {
+            while (true)
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine(" 1. Обмен USD в BYN");
+                Console.WriteLine(" 2. Обмен RUB в BYN");
+                Console.WriteLine(" 3. Обмен EUR в BYN");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(" 4. Выйти");
+                Console.ResetColor();
+                double sum;
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        sum = Usd / kursusd;
+                        Console.WriteLine($"BYN по отношению к USD:{sum}");
+                        break;
+                    case '2':
+                        sum = Rub/ kursrub;
+                        Console.WriteLine($"BYN по отношению к RUB:{sum}");
+                        break;
+                    case '3':
+                        sum = Eur / kurseur;
+                        Console.WriteLine($"BYN по отношению к EUR:{sum}");
+                        break;
+                    case '4':return;
+                    default: Console.Write("Вы ввели неправильную цифру"); break;
+                }
+            }
+        }
+        public void ExchangerBYN(double byn, double kursrub, double kursusd, double kurseur)
+        {
+            while (true)
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine(" 1. Обмен BYN в USD");
+                Console.WriteLine(" 2. Обмен BYN в RUB");
+                Console.WriteLine(" 3. Обмен BYN в EUR");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(" 4. Выйти");
+                Console.ResetColor();
+                double sum;
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        sum = byn * kursusd;
+                        Console.WriteLine($"USD по отношению к BYN:{sum}");
+                        break;
+                    case '2':
+                        sum =  byn * kursrub;
+                        Console.WriteLine($"RUB по отношению к BYN:{sum}");
+                        break;
+                    case '3':
+                        sum =  byn * kurseur;
+                        Console.WriteLine($"EUR по отношению к BYN:{sum}");
+                        break;
+                    case '4': return;
+                    default: Console.Write("Вы ввели неправильную цифру"); break;
+                }
+            }
+        }
+
+        static public Converter InPut()
+        {
+            Console.Write("Введите количество USD: ");
+            double usd = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите количество RUB: ");
+            double rub = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите количество EUR: ");
+            double eur = Convert.ToDouble(Console.ReadLine());
+            return new Converter(usd, rub, eur);
+        }
+    }
+}
---
+++ z5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Converter> list = new List<Converter>();
+            Converter converter = Converter.InPut();
+            list.Add(converter); 
+            Console.Write("Введите курс BYN к RUB: ");
+            double kursrub = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите курс BYN к USD: ");
+            double kursusd = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите курс BYN к EUR: ");
+            double kurseur = Convert.ToDouble(Console.ReadLine());
+            while (true) 
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine(" 1. из BYN в одну из указанных валют");
+                Console.WriteLine(" 2. из указанных валют в BYN");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(" 3. Выйти");
+                Console.ResetColor();
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        Console.WriteLine("Введите количество BYN: ");
+                        double byn = Convert.ToDouble(Console.ReadLine());
+                        converter.ExchangerBYN(byn, kursrub, kursusd, kurseur);
+                        break;
+                    case '2':
+                        converter.Exchange(kursrub, kursusd, kurseur);  
+                        break;
+                    case '3': return;
+                    default: Console.Write("Вы ввели неправильную цифру"); break;
+                }
+                Console.ReadLine();
+            }
+        }
+      
+    }
+}

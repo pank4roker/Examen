5c4a9362767522a3c60a390e72673f301d3550c4
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-05-24 14:17:38 +0300

    23

---
+++ z/Pattern.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z
+{
+    static class Pattern
+    {
+        public enum Colors
+        {
+            Black,
+            Blue,
+            Green,
+            Yellow,
+            Red,
+            Magenta,
+            Cyan,
+            White
+        }
+        public static void Print(string stroka, int color)
+        {
+            switch ((Colors)color)
+            {
+                case Colors.Black:
+                    Console.ForegroundColor = ConsoleColor.Black; break;
+                case Colors.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue; break;
+                case Colors.Green:
+                    Console.ForegroundColor = ConsoleColor.Green; break;
+                case Colors.Yellow:
+                    Console.ForegroundColor = ConsoleColor.Yellow; break;
+                case Colors.Red:
+                    Console.ForegroundColor = ConsoleColor.Red; break;
+                case Colors.Magenta:
+                    Console.ForegroundColor = ConsoleColor.Magenta; break;
+                case Colors.Cyan:
+                    Console.ForegroundColor = ConsoleColor.Cyan; break;
+                case Colors.White:
+                    Console.ForegroundColor = ConsoleColor.White; break;
+            }
+            Console.Write(stroka);
+
+        }
+    }
+}
---
+++ z/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string stroka = Console.ReadLine();
+            Console.WriteLine("Выберете доступный цвет:");
+            foreach (Pattern.Colors colors in Enum.GetValues(typeof(Pattern.Colors)))
+            {
+                Console.WriteLine($"{(int)colors} - {colors}");
+            }
+            Console.WriteLine("Введите цифру цвета:");
+            int color = int.Parse(Console.ReadLine());
+            Pattern.Print(stroka, color);
+            Console.ReadLine();
+        }
+    }
+}

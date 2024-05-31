4d4b125ca67f5167280a27dbd1cf435b8286a91e
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-05-31 14:22:42 +0300

    Add files via upload

---
+++ z29/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z29
+{
+    class Product
+    {
+        string name;
+        double price;
+        int count;
+        public Product() { }
+
+        public Product(string name, double price, int count)
+        {
+            Name = name;
+            Price = price;
+            Count = count;
+        }
+
+        public string Name { get => (name != "")?name:"Неизвестно"; set => name = value; }
+        public double Price { get => (price >= 1 && price<=20)?price:0; set => price = value; }
+        public int Count { get => (count>=0 && count <=10)?count:0; set => count = value; }
+
+        public void OutPut()
+        {
+            Console.WriteLine($"{Name}\n{Price}\n{Count}");
+        }
+        public double Sum()
+        {
+            return Price * Count;
+        }
+
+    }
+}
---
+++ z29/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            Product product = new Product("Стулья", 10, 5);
+            products.Add(product);
+            product.OutPut();
+            Console.WriteLine(product.Sum());
+            Product product1 = new Product("Столы", 15, 10);
+            products.Add(product1);
+            product1.OutPut();
+            Console.WriteLine(product1.Sum());
+            double sum = 0;
+            foreach(var elem in products)
+            {
+               sum += elem.Sum();
+            }
+            Console.Write($"Общая стоимость заказа: {sum}");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+     
+    }
+}

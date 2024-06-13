bbc0217b210a66a9addda9989eaae0e94a3e3849
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-06-13 22:37:24 +0300

    Add files via upload

---
+++ z17/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z17__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ISwitchable> switchables = new List<ISwitchable>();
+            TVSet tVSet = new TVSet();
+            PersonalComputer personalComputer = new PersonalComputer();
+            switchables.Add(tVSet);
+            switchables.Add(personalComputer);
+            foreach (var elem in switchables)
+            {
+                if (elem is TVSet)
+                {
+                    Console.WriteLine("Televisor");
+                    elem.On();
+                    elem.Off();
+                }
+                else
+                {
+                    Console.WriteLine("PC");
+                    elem.On();
+                    elem.Off();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}

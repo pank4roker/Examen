f51a5844a12743f32fb475b0f02d00e853a31057
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-06-14 20:50:42 +0300

    Add files via upload

---
+++ z19/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z18
+{
+    static class Calculator
+    {
+       public static int Add(int x,int y)
+       {
+            return x + y;
+       }
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Mul(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Div(int x, int y)
+        {
+           
+            
+                return x / y;   
+           
+           
+        }
+
+    }
+}
---
+++ z19/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Основные арифметические операции:");
+
+            int num1 = 5;
+            int num2 = 5;
+
+            Console.WriteLine($"{num1} + {num2} = {Calculator.Add(num1, num2)}");
+            Console.WriteLine($"{num1} - {num2} = {Calculator.Sub(num1, num2)}");
+            Console.WriteLine($"{num1} * {num2} = {Calculator.Mul(num1, num2)}");
+            if (num1 == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя");
+
+            }
+            else
+            {
+                Console.WriteLine($"{num1} / {num2} = {Calculator.Div(num1, num2)}");
+            }
+            // Для чтения ввода пользователя перед завершением программы
+            Console.ReadLine();
+
+        }
+    }
+}

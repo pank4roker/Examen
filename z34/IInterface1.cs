291d3ce6252170c6968da672bc7fa81b7ea7dc1f
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-06-14 22:52:43 +0300

    Add files via upload

---
+++ z34/IInterface1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z34
+{
+    interface IInterface1
+    {
+        void Method1();
+    }
+    interface IInterface2 : IInterface1
+    {
+        void Method2();
+    }
+    interface IInterface3 : IInterface2
+    {
+        void Method3();
+    }
+}
---
+++ z34/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.Method1();
+            derivedClass.Method2();
+            derivedClass.Method3();
+            Console.ReadLine();
+        }
+    }
+}

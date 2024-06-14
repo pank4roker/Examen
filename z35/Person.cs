764184e0b0a193c9480f2821d9ffd49e35f989b3
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-06-14 23:01:07 +0300

    Add files via upload

---
+++ z35/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z35
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        // Реализация метода CompareTo для сортировки по имени
+        public int CompareTo(Person other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        // Переопределение метода ToString для удобного вывода информации о человеке
+        public override string ToString()
+        {
+            return $"{Name}, {Age} лет";
+        }
+    }
+}
---
+++ z35/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> peopleList = new List<Person>
+            {
+                new Person("Янна", 28),
+                new Person("Борис", 35),
+                new Person("Вктор", 23),
+                new Person("алина", 31)
+            };
+
+            // Сортировка списка по имени
+            peopleList.Sort();
+
+            // Вывод отсортированного списка
+            Console.WriteLine("Отсортированный список людей:");
+            foreach (var person in peopleList)
+            {
+                Console.WriteLine(person);
+            }
+            Console.ReadLine();
+        }
+    }
+}
